d87fabbf9439ab4b5e9d8b772d90e38ea412282e
karinajankowska <37188718+karinajankowska@users.noreply.github.com>
2018-03-10 20:40:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,14 +18,28 @@ signed char schar1 = 4; // znak jako liczba całkowita
 unsigned char unschar1 = 4; // znak jako liczba całkowita
 float f1 = 2.5; // liczba rzeczywista
 double lf1 = 3.1412; // liczba rzeczywista
-printf ("Zmienna short int ma rozmiar: %dB\n", sizeof (sint1));
-printf ("Zmienna long int ma rozmiar: %dB\n", sizeof (lint1));
+unsigned short uns = 12; // cyfra bez znaku
+int n = 25; // liczba całkowita
+char c = 126; //znak jako liczba całkowita
+unsigned long unl = 4000; // długa liczba całkowita bez znaku
+long long ll = 90000; // długa liczba całkowita
+unsigned long long unll = 800; // długa liczba całkowita bez znaku
+long double ld = 7.2; // liczba rzeczywista
+printf ("Zmienna short int ma rozmiar: %dB\n, sizeof (sint1));
+printf ("Zmienna long int ma rozmiar: %dB\n, sizeof (lint1));
 printf ("Zmienna unsigned int ma rozmiar: %dB\n", sizeof (unsint1));
 printf ("Zmienna signed int ma rozmiar: %dB\n", sizeof (ssint1));
 printf ("Zmienna signed char ma rozmiar: %dB\n", sizeof (schar1));
 printf ("Zmienna unsigned char ma rozmiar: %dB\n", sizeof (unschar1));
 printf ("Zmienna float ma rozmiar: %dB\n", sizeof (f1));
 printf ("Zmienna double ma rozmiar: %dB\n", sizeof (lf1));
+printf ("Zmienna long double ma rozmiar: %dB\n", sizeof(ld));
+printf ("Zmienna unsigned long long ma rozmiar: %dB\n", sizeof (unll));
+printf ("Zmienna long long ma rozmiar: %dB\n", sizeof (ll));
+printf ("Zmienna unsigned long ma rozmiar: %dB\n", sizeof (unl));
+printf ("Zmienna char ma rozmiar: %dB\n", sizeof (n));
+printf ("Zmienna int ma rozmiar: %dB\n", sizeof(c));
+printf("Zmienna unsigned short ma rozmiar: %dB\n", sizeof(uns));
 
     return 0;
 }
