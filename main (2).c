24f56c5d241bc3ba24feb80c6f00acb2f7f60cb9
karinajankowska <37188718+karinajankowska@users.noreply.github.com>
2018-03-17 22:45:08 +0100

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-int const kelvin = 12;
+#define celsjusz\
 int const farenheit = 451;
 int main()
 {
