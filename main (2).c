dbcf76f2bb7a44cfb9311c81fba1aa6ea6164f54
karinajankowska <37188718+karinajankowska@users.noreply.github.com>
2018-03-17 22:29:57 +0100

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -9,8 +9,6 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 int const kelvin = 12;
 int const farenheit = 451;
-#define TRUE
-#define FALSE
 int main()
 {
 float celsjusz, fahrenheit, kelvin, rankine;
